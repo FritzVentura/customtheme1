69692f2a12e3ebdb0e2f52f81c950887c1f33838
FrederikKofoed <fk.kofoed@gmail.com>
2018-02-06 17:16:25 +0100

    Menu er opdater med Animationer

--- themes/systematik/static/js/nav.js
+++ themes/systematik/static/js/nav.js
@@ -1,3 +1,47 @@
+$(window).on("load", sidenErKlar);
+
+function sidenErKlar() {
+    console.log("Menuen er loadet");
+    
+    $("#nav_open").on("click", navInd);
+}
+
+function navInd() {
+    console.log("nav ind");
+    
+    $('#mySidenav').removeClass("sidenav_anim2");
+    
+    $('#mySidenav').addClass("sidenav_anim1");
+    
+    $("#nav_open").off("click", navInd);
+    
+    $("#mySidenav").on("click", navUd);
+}
+
+
+function navUd() {
+    console.log("nav ud");
+    
+    $('#nav_open').removeClass("sidenav_anim");
+    
+    $('#mySidenav').addClass("sidenav_anim2");
+    
+    $("#mySidenav").off("click", navUd);
+    
+    $("#nav_open").on("click", navInd);
+}
+
+
+/*function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+}*/
+
+// NAV DESKTOP
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
